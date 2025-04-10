c67175d8956a68d66dd9b11914bd6cc853a061bb
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-10 18:25:59 +0200

    Configure ModelMapper bean for centralized and reusable object mapping

---
+++ HotelBooking/src/main/java/com/claud/HotelBooking/config/ModelMapperConfig.java
@@ -0,0 +1,21 @@
+package com.claud.HotelBooking.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STANDARD);
+
+        return modelMapper;
+
+    }
+}
