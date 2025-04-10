1e5d6620d911e8dffdfd87a2d51a62b61497c5f4
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-10 19:29:30 +0200

    Entity Notification created

---
+++ HotelBooking/src/main/java/com/claud/HotelBooking/entities/Notification.java
@@ -0,0 +1,38 @@
+package com.claud.HotelBooking.entities;
+
+import com.claud.HotelBooking.enums.NotificationType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "notifications")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+
+    @NotBlank(message = "Recipient is required")
+    private String recipient;
+
+    private String body;
+
+    private String bookingReference;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
