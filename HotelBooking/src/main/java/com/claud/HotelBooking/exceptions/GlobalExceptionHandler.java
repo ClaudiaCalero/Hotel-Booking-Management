6e9cea3a4756ddf104180d4af8fa23371f1368b8
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-11 17:28:02 +0200

    GlobalExceptionHandler handlerAllExceptions corrected, handleNotFoundException + handleNameValueRequiredException +handleInvalidCredentialException + handleInvalidBookingStateAndDateException created

--- HotelBooking/src/main/java/com/claud/HotelBooking/exceptions/GlobalExceptionHandler.java
+++ HotelBooking/src/main/java/com/claud/HotelBooking/exceptions/GlobalExceptionHandler.java
@@ -13,7 +13,45 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Response> handleAllExceptions(Exception ex) {
         Response response = Response.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(ex.getMessage())
                 .build();
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Response> handleNotFoundException(NotFoundException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NameValueRequiredException.class)
+    public ResponseEntity<Response> handleNameValueRequiredException(NameValueRequiredException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidCredentialException.class)
+    public ResponseEntity<Response> handleInvalidCredentialException(InvalidCredentialException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidBookingStateAndDateException.class)
+    public ResponseEntity<Response> handleInvalidBookingStateAndDateException(InvalidBookingStateAndDateException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
