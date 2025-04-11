0d01112cc2ae46f2c9005dfda85c6327f41cb9e9
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-11 17:13:56 +0200

    NotFOundException created

---
+++ HotelBooking/src/main/java/com/claud/HotelBooking/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.claud.HotelBooking.exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message){
+        super(message);
+    }
+}
