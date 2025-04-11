3f76e53fcd0ced513eb033f1e77edde1b6c61622
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-11 14:23:52 +0200

    UserDTO created

---
+++ HotelBooking/src/main/java/com/claud/HotelBooking/dtos/UserDTO.java
@@ -0,0 +1,38 @@
+package com.claud.HotelBooking.dtos;
+
+import com.claud.HotelBooking.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    private String phoneNumber;
+
+    private UserRole role; //Customer, Admin
+
+    private Boolean isActive;
+    private LocalDateTime;
+
+
+}
