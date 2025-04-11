7ff9063cd8df8b46d048e44d4a2fe2667bdf59ba
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-11 17:15:25 +0200

    InvalidCredentialException created

---
+++ HotelBooking/src/main/java/com/claud/HotelBooking/exceptions/InvalidCredentialException.java
@@ -0,0 +1,8 @@
+package com.claud.HotelBooking.exceptions;
+
+public class InvalidCredentialException extends RuntimeException{
+
+    public InvalidCredentialException(String message){
+        super(message);
+    }
+}
