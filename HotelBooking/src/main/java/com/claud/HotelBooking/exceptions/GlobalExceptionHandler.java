b4572c2bb483067e4d0c31fc336bc422e8bd5a5e
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-04-11 17:20:30 +0200

    GlobalExceptionHandler created

---
+++ HotelBooking/src/main/java/com/claud/HotelBooking/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.claud.HotelBooking.exceptions;
+
+import com.claud.HotelBooking.dtos.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handleAllExceptions(Exception ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
