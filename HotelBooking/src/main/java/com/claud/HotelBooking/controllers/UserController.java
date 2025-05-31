47918424c25c5b417123878b36e94b0f11062def
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-05-31 12:03:29 +0200

    UserController + methods GET (All users, own account details and bokking history), PUT and DELETE

--- HotelBooking/src/main/java/com/claud/HotelBooking/controllers/UserController.java
+++ HotelBooking/src/main/java/com/claud/HotelBooking/controllers/UserController.java
@@ -1,4 +1,47 @@
 package com.claud.HotelBooking.controllers;
 
+import com.claud.HotelBooking.dtos.Response;
+import com.claud.HotelBooking.dtos.UserDTO;
+import com.claud.HotelBooking.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Response> updateOwnAccount(@RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.updateOwnAccount(userDTO));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Response> deleteOwnAccount(){
+        return ResponseEntity.ok(userService.deleteOwnAccount());
+    }
+
+    @GetMapping("/account")
+    public ResponseEntity<Response> getOwnAccountDetails(){
+        return ResponseEntity.ok(userService.getOwnAccountDetails());
+    }
+
+    @GetMapping("/bookings")
+    public ResponseEntity<Response> getMyBookingHistory(){
+        return ResponseEntity.ok(userService.getMyBookingHistory());
+    }
+
+
 }
+
+
