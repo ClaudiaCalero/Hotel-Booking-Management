7d1be2dd1bb239b625fa14124626ac36c814364a
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-07-15 12:28:35 +0200

    getAllRooms + getRoomById + deleteRoom + getAvailableRooms + getAllRoomTypes + SearchRoom http requests for RoomController

--- HotelBooking/src/main/java/com/claud/HotelBooking/controllers/RoomController.java
+++ HotelBooking/src/main/java/com/claud/HotelBooking/controllers/RoomController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/rooms")
@@ -59,6 +61,41 @@ public class RoomController {
 
         return ResponseEntity.ok(roomService.updateRoom(roomDTO,imageFile));
 
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Response> getAllRooms(){
+        return ResponseEntity.ok(roomService.getAllRooms());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getRoomById(@PathVariable Long id){
+        return ResponseEntity.ok(roomService.getRoomById(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> deleteRoom(@PathVariable Long id){
+        return ResponseEntity.ok(roomService.deleteRoom(id));
+    }
+
+    @GetMapping("/available")
+    public ResponseEntity<Response> getAvailableRooms(
+            @RequestParam LocalDate checkInDate,
+            @RequestParam LocalDate checkOutDate,
+            @RequestParam(required = false) RoomType roomType
+            ){
+        return ResponseEntity.ok(roomService.getAvailableRooms(checkInDate, checkOutDate, roomType));
+    }
+
+    @GetMapping("/types")
+    public ResponseEntity<List<RoomType>> getAllRoomTypes(){
+        return ResponseEntity.ok(roomService.getAllRoomTypes());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Response> searchRoom(@RequestParam String input){
+        return ResponseEntity.ok(roomService.searchRoom(input));
     }
 
 
