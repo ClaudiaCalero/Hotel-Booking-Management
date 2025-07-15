b706435f917d3f2989059e55c0357b3bdb796d9b
ClaudiaCalero <claudiaclrduro@gmail.com>
2025-07-15 12:06:28 +0200

    RestController created + first http request addRoom

---
+++ HotelBooking/src/main/java/com/claud/HotelBooking/controllers/RoomController.java
@@ -0,0 +1,45 @@
+package com.claud.HotelBooking.controllers;
+
+import com.claud.HotelBooking.dtos.Response;
+import com.claud.HotelBooking.dtos.RoomDTO;
+import com.claud.HotelBooking.enums.RoomType;
+import com.claud.HotelBooking.services.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/rooms")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> addRoom(@RequestParam Integer roomNumber,
+                                            @RequestParam RoomType type,
+                                            @RequestParam BigDecimal pricePerNight,
+                                            @RequestParam Integer capacity,
+                                            @RequestParam String description,
+                                            @RequestParam MultipartFile imageFile){
+        RoomDTO roomDTO = RoomDTO.builder()
+                .roomNumber(roomNumber)
+                .type(type)
+                .pricePerNight(pricePerNight)
+                .capacity(capacity)
+                .description(description)
+                .build();
+
+        return ResponseEntity.ok(roomService.addRoom(roomDTO,imageFile));
+
+    }
+
+}
