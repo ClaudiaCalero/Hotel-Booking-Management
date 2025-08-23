7bf150815c50e7a66607a691428d58c1a43e1ff6
unknown <claudiaclrduro@gmail.com>
2025-08-23 19:51:58 +0200

    ccreation of the folder payments and subfolders paypa, stripe and flutterwave. Inside Strip another sub called dto with a class Payment request. App properties modify to be able to use stripe+ PaymentService class done

--- HotelBooking/src/main/java/com/claud/HotelBooking/payments/stripe/PaymentService.java
+++ HotelBooking/src/main/java/com/claud/HotelBooking/payments/stripe/PaymentService.java
@@ -1,12 +1,26 @@
 package com.claud.HotelBooking.payments.stripe;
 
+import com.claud.HotelBooking.dtos.NotificationDTO;
+import com.claud.HotelBooking.entities.Booking;
+import com.claud.HotelBooking.entities.PaymentEntity;
+import com.claud.HotelBooking.enums.NotificationType;
+import com.claud.HotelBooking.enums.PaymentGateway;
+import com.claud.HotelBooking.enums.PaymentStatus;
+import com.claud.HotelBooking.exceptions.NotFoundException;
+import com.claud.HotelBooking.payments.stripe.dto.PaymentRequest;
 import com.claud.HotelBooking.repositories.BookingRepository;
 import com.claud.HotelBooking.repositories.PaymentRepository;
 import com.claud.HotelBooking.services.NotificationService;
+import com.stripe.Stripe;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -18,6 +32,88 @@ public class PaymentService {
     private final NotificationService notificationService;
 
     @Value("${stripe.api.secret.key}")
-    private String secretKey;
+    private String secreteKey;
+
+    public String createPaymentIntent(PaymentRequest paymentRequest){
+        log.info("Inside createPaymentIntent()");
+        Stripe.apiKey = secreteKey;
+        String bookingReference = paymentRequest.getBookingReference();
+
+
+        Booking booking = bookingRepository.findByBookingReference(bookingReference)
+                .orElseThrow(() -> new NotFoundException("Booking Not Found"));
+
+        if (booking.getPaymentStatus() == PaymentStatus.COMPLETED) {
+            throw new NotFoundException("Payment already made for this booking");
+
+        }
+
+        try{
+            PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
+                    .setAmount(paymentRequest.getAmount().multiply(BigDecimal.valueOf(100)).longValue()) //amount cents
+                    .setCurrency("usd")
+                    .putMetadata("bookingReference", bookingReference)
+                    .build();
+
+            PaymentIntent intent = PaymentIntent.create(params);
+            return intent.getClientSecret();
+
+        }catch (Exception e){
+            throw new RuntimeException("Error creating payment intent");
+        }
+
+    }
+
+    public void updatePaymentIntent(PaymentRequest paymentRequest){
+        log.info("Inside updatePaymentIntent()");
+        String bookingReference = paymentRequest.getBookingReference();
+
+        Booking booking = bookingRepository.findByBookingReference(bookingReference)
+                .orElseThrow(() -> new NotFoundException("Booking Not Found"));
+
+        PaymentEntity payment = new PaymentEntity();
+        payment.setPaymentGateway(PaymentGateway.STRIPE);
+        payment.setAmount(paymentRequest.getAmount());
+        payment.setTransactionId(paymentRequest.getTransactionId());
+        payment.setPaymentStatus(paymentRequest.isSuccess() ? PaymentStatus.COMPLETED : PaymentStatus.FAILED);
+        payment.setPaymentDate(LocalDateTime.now());
+        payment.setBookingReference(bookingReference);
+        payment.setUser(booking.getUser());
+
+        if (!paymentRequest.isSuccess()){
+            payment.setFailureReason(paymentRequest.getFailureReason());
+        }
+        paymentRepository.save(payment); //save payment to database
+
+        //create and send notifiaction
+        NotificationDTO notificationDTO = NotificationDTO.builder()
+                .recipient(booking.getUser().getEmail())
+                .type(NotificationType.EMAIL)
+                .bookingReference(bookingReference)
+                .build();
+
+        log.info("About to send notification inside updatePaymentBooking  by sms");
+
+        if (paymentRequest.isSuccess()){
+            booking.setPaymentStatus(PaymentStatus.COMPLETED);
+            bookingRepository.save(booking); //updated the booking
+
+            notificationDTO.setSubject("Booking Payment Successful");
+            notificationDTO.setBody("Congratulations!! Your payment for booking with reference: " + bookingReference + "is successful");
+            notificationService.sendEmail(notificationDTO); //send email
+
+        } else  {
+            booking.setPaymentStatus(PaymentStatus.FAILED);
+            bookingRepository.save(booking);//updated the booking
+
+            notificationDTO.setSubject("Booking Payment Failed");
+            notificationDTO.setBody("Your payment for booking with reference: " + bookingReference + "failed with reason: " + paymentRequest.getFailureReason());
+            notificationService.sendEmail(notificationDTO); //send email
+        }
+
+
+    }
+
+
 
 }
