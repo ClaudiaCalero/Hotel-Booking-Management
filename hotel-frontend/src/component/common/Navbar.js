2c425aef2c38a74afc52c77222587f01142e4407
unknown <claudiaclrduro@gmail.com>
2025-09-13 13:27:35 +0200

    static footer+navbar

--- hotel-frontend/src/component/common/Navbar.js
+++ hotel-frontend/src/component/common/Navbar.js
@@ -3,6 +3,7 @@ import { useNavigate, NavLink } from "react-router-dom";
 import ApiService from "../../service/ApiService";
 
 
+
 function Navbar() {
     const isAuthenticated = ApiService.isAuthenticated();
     const isCustomer = ApiService.isCustomer();
@@ -18,23 +19,24 @@ function Navbar() {
         }
     }
 
-    return (
+    return(
         <nav className="navbar">
             <div className="navbar-brand">
-                <NavLink to="/home">Onyx Crown Hotel</NavLink>
+                <NavLink to="/home"> Onyx Crown Hotel </NavLink>
             </div>
+
             <ul className="navbar-ul">
                 <li><NavLink to={"/home"} activeClassName="active">Home</NavLink></li>
                 <li><NavLink to={"/rooms"} activeClassName="active">Rooms</NavLink></li>
                 <li><NavLink to={"/find-booking"} activeClassName="active">Find My Bookings</NavLink></li>
 
-                {isCustomer && <li><NavLink to={"/profile"} activeClassName="active">Profile</NavLink></li>}
+                { isCustomer && <li><NavLink to={"/profile"} activeClassName="active">Profile</NavLink></li>}
                 {isAdmin && <li><NavLink to={"/admin"} activeClassName="active">Admin</NavLink></li>}
 
                 {!isAuthenticated && <li><NavLink to={"/login"} activeClassName="active">Login</NavLink></li>}
                 {!isAuthenticated && <li><NavLink to={"/register"} activeClassName="active">Register</NavLink></li>}
 
-                {!isAuthenticated && <li onClick={handleLogout}>Logout</li>}
+                {isAuthenticated && <li onClick={handleLogout}>Logout</li>}
             </ul>
         </nav>
     )
