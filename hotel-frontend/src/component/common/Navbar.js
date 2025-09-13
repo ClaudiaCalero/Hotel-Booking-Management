6208b7bec38f0e3389230a598577a57cd52e04bf
unknown <claudiaclrduro@gmail.com>
2025-09-13 12:58:56 +0200

    Navbar

---
+++ hotel-frontend/src/component/common/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+
+
+function Navbar() {
+    const isAuthenticated = ApiService.isAuthenticated();
+    const isCustomer = ApiService.isCustomer();
+    const isAdmin = ApiService.isAdmin();
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        const isLogout = window.confirm("Are you sure you want to logout?")
+        if (isLogout) {
+            ApiService.logout();
+            navigate("/home");
+        }
+    }
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <NavLink to="/home">Onyx Crown Hotel</NavLink>
+            </div>
+            <ul className="navbar-ul">
+                <li><NavLink to={"/home"} activeClassName="active">Home</NavLink></li>
+                <li><NavLink to={"/rooms"} activeClassName="active">Rooms</NavLink></li>
+                <li><NavLink to={"/find-booking"} activeClassName="active">Find My Bookings</NavLink></li>
+
+                {isCustomer && <li><NavLink to={"/profile"} activeClassName="active">Profile</NavLink></li>}
+                {isAdmin && <li><NavLink to={"/admin"} activeClassName="active">Admin</NavLink></li>}
+
+                {!isAuthenticated && <li><NavLink to={"/login"} activeClassName="active">Login</NavLink></li>}
+                {!isAuthenticated && <li><NavLink to={"/register"} activeClassName="active">Register</NavLink></li>}
+
+                {!isAuthenticated && <li onClick={handleLogout}>Logout</li>}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
