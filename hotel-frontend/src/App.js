673712b6f2df6e833c57d702ba52dfc36fd2cb9d
unknown <claudiaclrduro@gmail.com>
2025-11-07 17:20:05 +0100

    PaymentPage almost done

--- hotel-frontend/src/App.js
+++ hotel-frontend/src/App.js
@@ -8,31 +8,51 @@ import AllRoomsPage from "./component/booking_rooms/AllRoomsPage";
 import RoomDetailsPage from "../../../AppData/Local/Temp/0a4202d7-3e0d-4810-a16e-8fe57a9af885_HBS-react-spring.zip.885/HBS-react-spring/hotel-react-frontend/src/component/booking_rooms/RoomDetailsPage";
 import ProfilePage from "./component/profile/ProfilePage";
 import EditProfilePage from "./component/profile/EditProfilePage";
+import PaymentPage from "../../../AppData/Local/Temp/156e6b96-7589-47dc-82e6-82c1f3c99c26_HBS-react-spring.zip.c26/HBS-react-spring/hotel-react-frontend/src/component/payment/PaymentPage";
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar />
-        <div className="content">
-          <Routes>
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/rooms" element={<AllRoomsPage />} />
-            <Route path="/find-booking" element={<FindBookingPage />} />
-            <Route path="/rooms" element={<RoomDetailsPage />} />
+          <Navbar/>
+          <div className="content">
+              <Routes>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route exact path="/home" element={<HomePage/>}/>
+                <Route path="/rooms" element={<AllRoomsPage/>}/>
+                <Route path="/find-booking" element={<FindBookingPage/>}/>
 
-            <Route path="/room-details/:roomId" element={<CustomerRoute element={<RoomDetailsPage />} />} />
-            <Route path="/profile" element={<CustomerRoute element={<ProfilePage />} />} />
-            <Route path="/edit-profile" element={<CustomerRoute element={<EditProfilePage />} />} />
+                <Route path="/room-details/:roomId" element={<CustomerRoute element={<RoomDetailsPage/>}/>}/>
+                <Route path="/profile" element={<CustomerRoute element={<ProfilePage/>}/>}/>
+                <Route path="/edit-profile" element={<CustomerRoute element={<EditProfilePage/>}/>}/>
+
+                {/* PAYMENT PAGES */}
+
+                <Route path="/payment/:bookingReference/:amount" element={<CustomerRoute element={<PaymentPage/>}/>}/>
+                <Route path="/payment-success/:bookingReference"  element={<CustomerRoute element={<PaymentSuccess />} />} />
+                <Route path="/payment-failed/:bookingReference" element={<CustomerRoute element={<PaymentFailure />} />} />
+
+                {/* ADMIN ROUTES */}
+                <Route path="/admin" element={<AdminRoute element={<AdminPage/>}/>}/>
+                <Route path="/admin/manage-rooms" element={<AdminRoute element={<ManageRoomPage/>}/>}/>
+                <Route path="/admin/add-room" element={<AdminRoute element={<AddRoomPage/>}/>}/>
+                <Route path="/admin/edit-room/:roomId" element={<AdminRoute element={<EditRoomPage/>}/>}/>
+                <Route path="/admin/manage-bookings" element={<AdminRoute element={<ManageBookingsPage/>}/>}/>
+                <Route path="/admin/edit-booking/:bookingCode" element={<AdminRoute element={<EditBookingPage/>}/>}/>
 
 
+                <Route path="/admin-register" element={<AdminRoute element={<AdminRegisterPage/>}/>}/>
 
-          </Routes>
-        </div>
-        <Footer />
+
+                {/* FALLBACK URL */}
+                <Route path="*" element={<Navigate to={"/home"}/>}/>
+
+
+              </Routes>
+          </div>
+          <Footer/>
       </div>
     </BrowserRouter>
   );
