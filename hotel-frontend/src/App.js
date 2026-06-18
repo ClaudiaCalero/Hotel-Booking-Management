bb1f7b4feb5350ae94734bcdd85a1632b7497e9f
unknown <claudiaclrduro@gmail.com>
2026-06-18 11:39:20 +0200

    room details page DONE

--- hotel-frontend/src/App.js
+++ hotel-frontend/src/App.js
@@ -7,13 +7,14 @@ import LoginPage from "./component/auth/LoginPage";
 import HomePage from "./component/home/HomePage";
 import AllRoomsPage from "./component/booking_rooms/AllRoomsPage";
 import RoomDetailsPage from "./component/booking_rooms/RoomDetailsPage";
-import {CustomerRoute} from "./service/Guard";
+import {AdminRoute, CustomerRoute} from "./service/Guard";
 import FindBookingPage from "./component/booking_rooms/FindBookingPage";
 import ProfilePage from "./component/profile/ProfilePage";
 import EditProfilePage from "./component/profile/EditProfilePage";
 import PaymentPage from "./component/payments/PaymentPage";
 import PaymentSuccess from "./component/payments/PaymentSuccess";
 import PaymentFailure from "./component/payments/PaymentFailure";
+import AdminPage from "./component/admin/AdminPage";
 
 
 
