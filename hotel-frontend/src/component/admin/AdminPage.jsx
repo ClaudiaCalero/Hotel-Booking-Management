19378c66153259def431f0a383caf295537ee3d2
unknown <claudiaclrduro@gmail.com>
2026-09-07 20:43:28 +0200

    admin page with css done, fixed the problem with the ApiService response about the admin name

--- hotel-frontend/src/component/admin/AdminPage.jsx
+++ hotel-frontend/src/component/admin/AdminPage.jsx
@@ -1,38 +1,53 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import ApiService from '../../service/ApiService';
-
-
+import ApiService from "../../service/ApiService";
 
 const AdminPage = () => {
+  const [adminName, setAdminName] = useState("");
+  const navigate = useNavigate();
 
-    const [adminName, setAdminName] = useState('');
-    const navigate = useNavigate()
+  useEffect(() => {
+    const fetchAdminName = async () => {
+      try {
+        const resp = await ApiService.myProfile();
+        console.log("Respuesta de la API recibida:", resp);
 
-
-    useEffect(() =>{
-        const fetchAdminName = async () => {
-            try {
-                const resp = await ApiService.myProfile();
-                setAdminName(resp.user.firstName)
-                
-            } catch (error) {
-                console.log(error.message)
-            }
+        if (resp && resp.user) {
+          setAdminName(resp.user.firstName);
+        } else if (resp && resp.firstName) {
+          setAdminName(resp.firstName);
         }
-        fetchAdminName();
-    }, []);
+      } catch (error) {
+        console.log("Error al cargar perfil:", error.message);
+      }
+    };
+    fetchAdminName();
+  }, []);
 
-
-    return(
-        <div className="admin-page">
-            <h1 className="welcome-message">Welcome, {adminName}</h1>
-            <div className="admin-actions">
-                <button className="admin-button" onClick={()=> navigate('/admin/manage-rooms')}> Manage Rooms</button>
-                <button className="admin-button" onClick={()=> navigate('/admin/manage-bookings')}> Manage Bookings</button>
-            </div>
+  return (
+    <div className="admin-page-container">
+      <div className="admin-background-wrapper">
+        <div className="admin-page-container">
+          <h1 className="welcome-message">Welcome, {adminName}</h1>
+          <div className="admin-actions">
+            <button
+              className="admin-button"
+              onClick={() => navigate("/admin/manage-rooms")}
+            >
+              {" "}
+              Manage Rooms
+            </button>
+            <button
+              className="admin-button"
+              onClick={() => navigate("/admin/manage-bookings")}
+            >
+              {" "}
+              Manage Bookings
+            </button>
+          </div>
         </div>
-    )
-
-}
+      </div>
+    </div>
+  );
+};
 export default AdminPage;
